dd772159768c9aa7e3162633773e6c5e78486c38
NotTheMindProbe <Izzytotheh@gmail.com>
2024-03-28 11:54:35 -0500

    Add raise/lower tubing servo to receiving Pi
    In conjunction with the MGKcalling update, this code takes the new servo2_position value and applies it to GPIO pin 23, which should move the position of an attached servo motor when button 5 (right bumper) is pressed.

--- MGKSending.py
+++ MGKSending.py
@@ -13,6 +13,7 @@ factory = PiGPIOFactory()
 brushless = Servo(27, min_pulse_width = 0.5/1000, max_pulse_width = 2.5/1000, pin_factory = factory)
 servo = Servo(17, min_pulse_width = 0.5/1000, max_pulse_width = 2.5/1000, pin_factory = factory)
 pump = Servo(22, min_pulse_width = 0.5/1000, max_pulse_width = 2.5/1000, pin_factory = factory)
+pump_servo = Servo(23, min_pulse_width = 0.5/1000, max_pulse_width = 2.5/1000, pin_factory = factory)
 
 while True:
     data, addr = sock.recvfrom(1024)
@@ -22,9 +23,11 @@ while True:
     brushless_value = controller_data["brushless_value"]
     servo_value = controller_data["servo_value"]
     pump_value = controller_data["pump_value"]
+    servo2_value = controller_data["servo2_position"]
     
     brushless.value = brushless_value
     servo.value = servo_value
     pump.value = pump_value
+    pump_servo.value = servo2_value
     
     print(controller_data)
